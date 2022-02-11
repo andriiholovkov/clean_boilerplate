fd3c3c413d643dc25d332c5160812fec9dcd380a
Denys Diedov <denis2016wolverine2016@gmail.com>
2022-02-11 18:13:17 +0200

    11-02-22: finish one more page of desktop layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,17 @@
 import React from 'react';
 import './App.css';
 // import HomePage from './modules/HomePage/HomePage';
-import TypeSupplement from './modules/Quiz/TypeSupplement/TypeSupplement';
+// import TypeSupplement from './modules/Quiz/TypeSupplement/TypeSupplement';
 // import Question from './modules/Quiz/Question/Question';
+import MoreQuestions from './modules/Quiz/MoreQuestions/MoreQuestions';
 
 function App() {
   return (
     <div className="App">
       {/*<HomePage />*/}
-      <TypeSupplement />
+      {/*<TypeSupplement />*/}
       {/*<Question />*/}
+      <MoreQuestions />
     </div>
   );
 }
